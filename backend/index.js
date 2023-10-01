04e1551f10febd5356c5d65afac3ccc4b7b4ac59
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-02 04:24:50 +0700

    add react with vite

--- backend/index.js
+++ backend/index.js
@@ -2,44 +2,31 @@ import express, { response } from 'express';
 import {PORT, mongoDBURL} from './config.js';
 import mongoose, { Mongoose } from 'mongoose';
 import {Book} from './models/book-model.js';
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origin with Default of cors(*)
+// app.use(cors())
+// Option 2: Allow costum Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(200).send('Welcome to MERN Stack Tutorial')
 });
 
-// Route for Save new book
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
 .connect(mongoDBURL)
